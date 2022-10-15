c9978bf475b0b0b49a1daedaebac58e50ed64fab
Cronus1007 <lit2019058@iiitl.ac.in>
2022-10-15 17:09:05 +0530

    message

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const cookieparser = require('cookie-parser');
 
 // import internal modules
 // const userRoute = require('./routes/user');
-// const hotelRoute = require('./routes/hotel');
-// const roomRoute = require('./routes/room');
+const hotelRoute = require('./routes/hotel');
+const roomRoute = require('./routes/room');
 // const blogRoute = require('./routes/blog');
 
 const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cors());
-// app.use(cookieparser());
-// dotenv.config();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(cookieparser());
+dotenv.config();
 
 // database connection
 mongoose
@@ -32,29 +32,29 @@ mongoose
 
 // application routing
 // app.use('/api', userRoute);
-// app.use('/api', hotelRoute);
-// app.use('/api', roomRoute);
+app.use('/api', hotelRoute);
+app.use('/api', roomRoute);
 // app.use('/api', blogRoute);
 
-// // home route
-// app.use((req, res) => {
-//     res.status(200).json({
-//         message: 'server running.',
-//     });
-// });
+// home route
+app.use((req, res) => {
+    res.status(200).json({
+        message: 'server running.',
+    });
+});
 
-// // not found handler
-// app.use((req, res, next) => {
-//     res.status(404).json({
-//         error: 'URL not Found',
-//     });
-// });
-// // error handling
-// app.use((err, req, res, next) => {
-//     res.status(500).json({
-//         error: err,
-//     });
-// });
+// not found handler
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: 'URL not Found',
+    });
+});
+// error handling
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        error: err,
+    });
+});
 
 const port = process.env.PORT || 4000;
 // const host = process.env.HOST || '0.0.0.0';
