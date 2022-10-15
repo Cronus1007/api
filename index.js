aad53f768eeaa043e0adb1fbf138b0b02243d329
Cronus1007 <lit2019058@iiitl.ac.in>
2022-10-15 17:02:42 +0530

    message

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const mongoose = require('mongoose');
 const cookieparser = require('cookie-parser');
 
 // import internal modules
-const userRoute = require('./routes/user');
-const hotelRoute = require('./routes/hotel');
-const roomRoute = require('./routes/room');
-const blogRoute = require('./routes/blog');
+// const userRoute = require('./routes/user');
+// const hotelRoute = require('./routes/hotel');
+// const roomRoute = require('./routes/room');
+// const blogRoute = require('./routes/blog');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-app.use(cookieparser());
-dotenv.config();
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cors());
+// app.use(cookieparser());
+// dotenv.config();
 
 // database connection
 mongoose
-    .connect('mongodb://localhost:27017', {
+    .connect('mongodb://localhost:27017/travelback', {
         useUnifiedTopology: true,
     })
     .then(() => {
@@ -31,30 +31,30 @@ mongoose
     });
 
 // application routing
-app.use('/api', userRoute);
-app.use('/api', hotelRoute);
-app.use('/api', roomRoute);
-app.use('/api', blogRoute);
+// app.use('/api', userRoute);
+// app.use('/api', hotelRoute);
+// app.use('/api', roomRoute);
+// app.use('/api', blogRoute);
 
-// home route
-app.use((req, res) => {
-    res.status(200).json({
-        message: 'server running.',
-    });
-});
+// // home route
+// app.use((req, res) => {
+//     res.status(200).json({
+//         message: 'server running.',
+//     });
+// });
 
-// not found handler
-app.use((req, res, next) => {
-    res.status(404).json({
-        error: 'URL not Found',
-    });
-});
-// error handling
-app.use((err, req, res, next) => {
-    res.status(500).json({
-        error: err,
-    });
-});
+// // not found handler
+// app.use((req, res, next) => {
+//     res.status(404).json({
+//         error: 'URL not Found',
+//     });
+// });
+// // error handling
+// app.use((err, req, res, next) => {
+//     res.status(500).json({
+//         error: err,
+//     });
+// });
 
 const port = process.env.PORT || 4000;
 // const host = process.env.HOST || '0.0.0.0';
